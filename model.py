730d2117c984a27caeb86a0e30032175dd80b4b6
Pankaj Kumar <pankaj.kumar@iongroup.com>
2018-03-03 12:13:20 +0530

    Made changes to model
    Changed model similar to NVidia. Added recovery images

--- model.py
+++ model.py
@@ -1,61 +1,142 @@
+import logging
 import csv
 import cv2
 import numpy as np
 import os
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Activation , MaxPooling2D, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Activation, MaxPooling2D, Cropping2D, Dropout
 from keras.layers import Convolution2D
 
-lines = []
 
-with open('data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader,None)
-    for line in reader:
-        lines.append(line)
+def create_logger():
+    logger = logging.getLogger(__name__)
+
+    logger.setLevel(logging.DEBUG)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+
+    return logger
+
+
+logger = create_logger()
+
+DRIVING_LOG_CSV = 'driving_log.csv'
 
 images = []
 measurements = []
 
-file_sep = os.sep
-for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = 'data/IMG/' + filename
+
+def load_image(center_source_path, measurement, data='data'):
+    filename = center_source_path.split('/')[-1]
+    current_path = data + '/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
-    measurement = float(line[3])
     measurements.append(measurement)
 
-augmented_images, augmented_measurments = [], []
 
-for image, measurement in zip(images, measurements) :
-    augmented_images.append(image)
-    augmented_measurments.append(measurement)
-    augmented_images.append(cv2.flip(image,1))
-    augmented_measurments.append(measurement*-1.0)
+def load_images(data_folder, skip_header=False, left=True, right=True):
+    lines = []
+    with open('%s/%s' % (data_folder, DRIVING_LOG_CSV)) as csvfile:
+        reader = csv.reader(csvfile)
+        if skip_header:
+            next(reader, None)
+        for line in reader:
+            lines.append(line)
+
+    for line in lines:
+        load_image(line[0], float(line[3]), data_folder)
+        if left:
+            load_image(line[1], float(line[3]) + 0.2, data_folder)
+        if right:
+            load_image(line[2], float(line[3]) - 0.2, data_folder)
+
+
+logger.info('Loading images from data folder')
+
+load_images('data', skip_header=True, left=True, right=True)
+logger.info('Total Loaded images %s', len(images))
+
+logger.info('Loading images from to_center folder')
+
+load_images('to_center', skip_header=False, left=True, right=False)
+logger.info('Total Loaded images %s', len(images))
+
+logger.info('Loading images from right_to_center folder')
+
+load_images('right_to_center', skip_header=False, left=False, right=False)
+logger.info('Total Loaded images %s', len(images))
+
+
+def augment_images():
+    augmented_images, augmented_measurments = [], []
+    logger.info('Augementing Images ....')
+    for image, measurement in zip(images, measurements):
+        augmented_images.append(image)
+        augmented_measurments.append(measurement)
+        augmented_images.append(cv2.flip(image, 1))
+        augmented_measurments.append(measurement * -1.0)
+    return  augmented_images,augmented_measurments
+
+
+augmented_images, augmented_measurments = augment_images()
+
+logger.info('Total Loaded images after augmentation %s', len(augmented_images))
 
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurments)
 
 
-model = Sequential()
-model.add(Lambda(lambda x: x / 255.0, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((70,25),(0,0))))
+def model():
+    """
+    Model Description -
+    Convolution Layers
 
-model.add(Convolution2D(24, 5, 5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(36, 5, 5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(48, 5, 5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
+    1) 24 5*5 conv layer with Stride of 2 with activation Relu
+    2) 36 5*5 conv layer with Stride of 2 with activation Relu
+    3) 48 5*5 conv layer with Stride of 2 with activation Relu
+    4) 64 3*3 conv layer with activation Relu
+    5) 64 3*3 conv layer with activation Relu
 
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(1))
+    Fully connected Layers
+    1) FC Layer - 100 dimension
+    2) FC Layer - 50 dimension
+    3) Output Layer - 1
+
+    :return:
+    """
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255.0, input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(1))
+
+    return model
+
+
+model = model()
 
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True,nb_epoch=5)
-print("Saving Model ............")
+
+logger.info('Running model with error function as mse and optimizer as Adam')
+
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+
+logger.info('Saving Model ............')
+
+
 model.save('model.h5')
